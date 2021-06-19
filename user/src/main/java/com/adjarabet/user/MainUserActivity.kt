e4225861924792ee4e6a8fbd639d5d443f81d4c2
nchan17 <nchan17@freeuni.edu.ge>
2021-06-19 20:20:19 +0400

    Initial commit, create two modules

---
+++ user/src/main/java/com/adjarabet/user/MainUserActivity.kt
@@ -0,0 +1,11 @@
+package com.adjarabet.user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainUserActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_user)
+    }
+}
